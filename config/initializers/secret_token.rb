496eb4c09ba83cb2e8db176141fdf4b081c9920a
saltava <saltava@fadrell.com>
2011-04-06 15:13:25 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trabajos::Application.config.secret_token = '9c19d4568d7ae2cc15ca522fb37fd9c0ba9e5794a0d3d9caa84e4e509cb150109204ee11947f214bbf673519f5cb4bcd4aa13136b2360e18d7f36f6440471dc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Trabajos::Application.config.session_store :cookie_store, :key => '_trabajos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Trabajos::Application.config.session_store :active_record_store
